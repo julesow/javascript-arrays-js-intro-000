0f4e57b37ad031ca95fb860a85b128610fb8b845
SOULEYMANE SOW <souleymanesow@gmail.com>
2018-10-16 03:44:19 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -15,7 +15,7 @@ function destructivelyAddElementToBeginningOfArray(array,el){
 
 function addElementToEndOfArray (array,el){
   
-[...array,el];
+return [...array,el];
 }
 
 function destructivelyAddElementToEndOfArray(array,el){
