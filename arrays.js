bd0bba60d075e86aa9c7d98ad52f598b69d33f3c
SOULEYMANE SOW <souleymanesow@gmail.com>
2018-10-16 03:30:55 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,47 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
+function addElementToBeginningOfArray (array,el){
+  
+  array = [el,...array]
+  return array
+  
+}
+
+function destructivelyAddElementToBeginningOfArray(array,el){
+  array.unshift(el);
+  return array
+  
+}
+
+function addElementToEndOfArray (array,el){
+  
+  array = [array,...el]
+
+  
+}
+
+function destructivelyAddElementToEndOfArray(array,el){
+  array.push(el);
+  return array
+  
+}
+
+
+function accessElementInArray(array,i){
+  return array[i];
+  
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array){ array.shift(); return array}
+
+function removeElementFromBeginningOfArray(array){array.slice(1);}
+
+
+function destructivelyRemoveElementFromEndOfArray(array){
+  array.pop();
+}
+
+
+function removeElementFromEndOfArray(array){array.slice(0,removeElementFromEndOfArray.length-1)
+  
+}
